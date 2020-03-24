19293ad75fe619ebe38babf3cb31cb9544c02294
RabbitWhite1 <895650485@qq.com>
2020-03-24 20:54:17 +0800

    add redirection

--- lab2/shell.c
+++ lab2/shell.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include<fcntl.h>
 
 #define INPUT_SIZE 256
 #define PWD_SIZE 4096
@@ -17,21 +19,22 @@
 void split_strs(char* str, const char* sep, /* in */
                 char** strs, int* strc /* out */);
 void exec_cmd(char* cmd, int will_fork);
+char* strip(char* str, const char* chars);
+
 
 int main() {
-    /* the input from the shell */
-    char input[INPUT_SIZE];
-    /* save the commands if pipe is used */
-    char** cmds = (char**)calloc(CMD_NUM, sizeof(char*));
+    /* current work directory */
+    char cwd[PWD_SIZE];
     /* count for the commands */
     int cmdc = 0;
 
     while (1) {
+        /* the input from the shell */
+        char* input = (char*)calloc(INPUT_SIZE, sizeof(char));
         /* command prompt */
-        char cwd[PWD_SIZE];
         getcwd(cwd, PWD_SIZE);
-        printf("%s# ", cwd);
-
+        printf("\033[1m\033[32mRabbit\033[0m:\033[1m\033[35m%s\033[0m# ", cwd);
+        
         /* get the input */
         fflush(stdin);
         fgets(input, INPUT_SIZE, stdin);
@@ -44,7 +47,7 @@ int main() {
 
         /* pipe used in input */
         /* get the commands */
-        char** cmds = (char**)calloc(100, sizeof(char*));
+        char** cmds = (char**)calloc(CMD_NUM, sizeof(char*));
         int cmdc = 0;
         split_strs(input, "|\n", cmds, &cmdc);
 
@@ -74,7 +77,6 @@ int main() {
                 if(i < cmdc - 1) {
                     dup2(fds[i][1], 1);
                 }
-
                 /* execute the command */
                 exec_cmd(cmds[i], false);
             }
@@ -85,20 +87,20 @@ int main() {
                 }
             }
             free(args);
-        }     
+        }
+        free(cmds);
     }
-    free(cmds);   
 
     return 0;
 }
 
-/* describe: this function is used for split the str by sep into strs, 
- * while the count of strs will be returned by strc
+/* Describe: this function is used for split the str by sep into strs, 
+ *  while the count of strs will be returned by strc
  * 
- * input:
+ * Input:
  *  str: string to be split
  *  sep: split by sep
- * output:
+ * Output:
  *  strs: the split strs
  *  strc: the count of split strs
  */ 
@@ -113,12 +115,71 @@ void split_strs(char* str, const char* sep, /* in */
     strs[*strc] = NULL;
 }
 
-/* describe: execute a command, but without pipe "|"
- * input:
+/* Describe: set suffixal char into '\0', 
+ *  and move str to the first meaningful position.
+ * Input:
+ *  str: string to be stripped
+ *  chars: the char to be stripped
+ * Return:
+ *  the stripped string.
+ */ 
+char* strip(char* str, const char* chars) {
+    char temp[2] = "a";
+    int len = strlen(str);
+    for(int i = 0; i < len; i++) {
+        temp[0] = str[0];
+        if(strstr(chars, temp)) {
+            str++;
+        }
+        else {
+            break;
+        }
+    }
+    len = strlen(str);
+    for(int i = len-1; i >= 0; i--) {
+        temp[0] = str[i];
+        if(strstr(chars, temp)) {
+            str[i] = '\0';
+        }
+        else {
+            break;
+        }
+    }
+    return str;
+}
+
+/* Describe: execute a command, but without pipe "|".
+ *  But <, >, >> are allowed.
+ * Input:
  *  cmd: the command to be execute
  *  will_fork: whether the external command will execute in a child process
  */ 
 void exec_cmd(char* cmd, int will_fork) {
+    /* check if <, >, >> in command */
+    char* out_fname = NULL;
+    int out_flag = 0;
+    if(out_fname = strstr(cmd, ">>")) {
+        *(out_fname) = '\0';
+        out_fname = out_fname + 2;
+        out_fname = strip(out_fname, " \n");
+        out_flag = 2;
+    }
+    else if(out_fname = strstr(cmd, ">")) {
+        *(out_fname) = '\0';
+        out_fname = out_fname + 1;
+        out_fname = strip(out_fname, " \n");
+        out_flag = 1;
+    }
+    char* in_fname = NULL;
+    int in_flag = 0;
+    if(in_fname = strstr(cmd, "<")) {
+        *(in_fname) = '\0';
+        in_fname = in_fname + 1;
+        in_fname = strip(in_fname, " \n");
+        in_flag = 1;
+    }
+
+    /* dealing command */
     int argc = 0;
     char** args = (char**)calloc(ARG_NUM, sizeof(char*));
     split_strs(cmd, " \n", args, &argc);
@@ -154,21 +215,51 @@ void exec_cmd(char* cmd, int will_fork) {
         exit(0);
 
     /* external command */
+    int fd;
     if(will_fork) {
         pid_t pid = fork();
         if (pid == 0) {
+            /* if >, >>, < is used */
+            if(out_flag == 2) {
+                close(1);
+                fd = open(out_fname, O_RDWR|O_CREAT|O_APPEND, 0666);
+            }
+            else if(out_flag == 1) {
+                close(1);
+                fd = open(out_fname, O_WRONLY|O_CREAT|O_TRUNC, 0666);
+            }
+            if(in_flag == 1) {
+                close(0);
+                fd = open(in_fname, O_RDONLY);
+            }
             /* child process */
             execvp(args[0], args);
             /* execvp failed */
             exit(255);
         }
-        /* parent process */
-        wait(NULL);
+        else {
+            /* parent process */
+            wait(NULL);
+        }
     }
     else {
+        /* if >, >>, < is used */
+        if(out_flag == 2) {
+            close(1);
+            fd = open(out_fname, O_RDWR|O_CREAT|O_APPEND, 0666);
+        }
+        else if(out_flag == 1) {
+            close(1);
+            fd = open(out_fname, O_WRONLY|O_CREAT|O_TRUNC, 0666);
+        }
+        if(in_flag == 1) {
+            close(0);
+            fd = open(in_fname, O_RDONLY);
+        }
         /* child process */
         execvp(args[0], args);
         /* execvp failed */
         exit(255);
     }
 }
+
