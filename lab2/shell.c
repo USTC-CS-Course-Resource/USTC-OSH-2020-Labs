2d69f5cb2d05d6dd6287e69561c50bce32292a79
RabbitWhite1 <895650485@qq.com>
2020-03-24 12:07:53 +0800

    add annotation

--- lab2/shell.c
+++ lab2/shell.c
@@ -5,58 +5,66 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
+#define INPUT_SIZE 256
 #define PWD_SIZE 4096
 #define CMD_SIZE 256
 #define ARG_NUM 128
 #define CMD_NUM 128
 
-void split_strs(char* str, char* sep, /* in */
+void split_strs(char* str, const char* sep, /* in */
                 char** strs, int* strc /* out */);
 void exec_cmd(char* cmd);
 
 int main() {
-    /* 输入的命令行 */
-    char input[CMD_SIZE];
-    /* 命令行拆解成的各部分，以空指针结尾 */
+    /* the input from the shell */
+    char input[INPUT_SIZE];
+    /* save the commands if pipe is used */
     char** cmds = (char**)calloc(CMD_NUM, sizeof(char*));
+    /* count for the commands */
     int cmdc = 0;
 
     while (1) {
-        /* 提示符 */
+        /* command prompt */
         char cwd[PWD_SIZE];
         getcwd(cwd, PWD_SIZE);
         printf("%s# ", cwd);
 
-        /* 读取输入 */
+        /* get the input */
         fflush(stdin);
-        fgets(input, CMD_SIZE, stdin);
-        /* 判断input中是否使用了管道，如未使用则直接执行 */
+        fgets(input, INPUT_SIZE, stdin);
+
+        /* check if pipe isn't used in input */
         if(!strstr(input, "|")) {
             exec_cmd(input);
             continue;
         }
-        /* input中使用了管道 */
+
+        /* pipe used in input */
+        /* get the commands */
         char** cmds = (char**)calloc(100, sizeof(char*));
         int cmdc = 0;
-        char sep[] = "|\n";
-        split_strs(input, sep, cmds, &cmdc);
+        split_strs(input, "|\n", cmds, &cmdc);
+
+        /* initialize the pipes */
         int fds[cmdc-1][2];
         for(int i = 0; i < cmdc-1; i++) {
             pipe(fds[i]);
         }
         
-        pid_t pid;
-        
+        /* execute and connect the pipes */
         for(int i = 0; i < cmdc; i++) {
+            /* get split args of each command */
             char** args = (char**)calloc(ARG_NUM, sizeof(char*));
             int argc = 0;
-            char sep[] = " \n";
+
             pid_t pid = fork();
 
             if(pid < 0) {
                 printf("error: can't fork!\n");
+                exit(2);
             }
             else if(pid == 0) {
+                /* connect the pipes */
                 if(i != 0) {
                     dup2(fds[i-1][0], 0);
                     close(fds[i-1][0]);
@@ -66,8 +74,8 @@ int main() {
                     dup2(fds[i][1], 1);
                 }
 
+                /* execute the command */
                 exec_cmd(cmds[i]);
-                execvp(args[0], args);
             }
             else {
                 wait(NULL);
@@ -86,7 +94,7 @@ int main() {
 /* this function is used for split the str by sep into strs, 
  * while the count of strs will be returned by strc
  */ 
-void split_strs(char* str, char* sep, /* in */
+void split_strs(char* str, const char* sep, /* in */
                 char** strs, int* strc /* out */) {
     *strc = 0;
     char* token = strtok(str, sep);
@@ -100,13 +108,12 @@ void split_strs(char* str, char* sep, /* in */
 void exec_cmd(char* cmd) {
     int argc = 0;
     char** args = (char**)calloc(ARG_NUM, sizeof(char*));
-    char sep[] = " \n";
-    split_strs(cmd, sep, args, &argc);
-    /* 没有输入命令 */
+    split_strs(cmd, " \n", args, &argc);
+    /* no command */
     if (!args[0])
         return;
 
-    /* 内建命令 */
+    /* built-in command */
     if (strcmp(args[0], "cd") == 0) {
         if (args[1])
             chdir(args[1]);
@@ -119,7 +126,7 @@ void exec_cmd(char* cmd) {
     }
     if (strcmp(args[0], "export") == 0) {
         for (int i = 1; args[i] != NULL; i++) {
-            /*处理每个变量*/
+            /* deal with each variables */
             char *name = args[i];
             char *value = args[i] + 1;
             while (*value != '\0' && *value != '=')
