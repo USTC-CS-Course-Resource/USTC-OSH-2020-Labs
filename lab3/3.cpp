999cc8f2063df9fcdc1070dd15b41c2a5ec40cba
Zhanghan Wang <895650485@qq.com>
2020-05-02 13:26:21 +0800

    reformat 3.cpp

--- lab3/3.cpp
+++ lab3/3.cpp
@@ -41,27 +41,8 @@ public:
         msgs.clear();
     }
     
-    void feed(string data, int from) {
-        buffer += data;
-        // 按照'\n'分割字符串
-        while(true) {
-            size_t pos = buffer.find('\n');
-            if(pos == string::npos) break;
-            char prompt[PROMPT_SIZE];
-            if(from != -2) sprintf(prompt, "[Message from %2d] ", from);
-            else memset(prompt, 0, PROMPT_SIZE*sizeof(char));
-            msgs.push_back(Message(string(prompt) + buffer.substr(0UL, pos + 1), from));
-            if(pos + 1 == buffer.length()) buffer = string();
-            else buffer = buffer.substr(pos + 1);
-        }
-    }
-
-    Message get_one_msg() {
-        if(msgs.size() <= 0) return Message();
-        Message msg = msgs[0];
-        msgs.erase(msgs.begin());
-        return msg;
-    }
+    void feed(string data, int from);
+    Message get_one_msg();
 };
 
 class Client {
@@ -72,31 +53,9 @@ public:
     MessageReader reader;
     string send_queue;
 
-    Client(int fd) {
-        this->fd = fd;
-    }
-
-    string recv_some(fd_set* recv_clients) {
-        if(!FD_ISSET(fd, recv_clients)) return string();
-        char* buffer = new char[BUF_SIZE+1];
-        recv(fd, buffer, BUF_SIZE, 0);
-        string string_buffer = string(buffer);
-        delete [] buffer;
-        return string_buffer;
-    }
-
-    void send_some(fd_set* send_clients) {
-        if(!FD_ISSET(fd, send_clients)) return; // 不能发送, 返回
-        Message msg;
-        if(send_queue.length() == 0) {
-            msg = reader.get_one_msg();
-            send_queue = msg.buffer;
-        }
-        if(send_queue.length() == 0) return; // 没有可发送消息, 返回
-        ssize_t sent_size = send(fd, send_queue.c_str(), send_queue.length(), MSG_NOSIGNAL);
-        if(sent_size == -1) return;
-        send_queue = send_queue.substr(sent_size);
-    }
+    Client(int fd) : fd(fd) {};
+    string recv_some(fd_set* recv_clients);
+    void send_some(fd_set* send_clients);
 };
 
 class Server {
@@ -107,120 +66,19 @@ public:
     int max_fd;
     int fd;
 
-    Server(int port) {
-        if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-            perror("socket");
-            exit(1);
-        }
-
-        int on = 1;
-        setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int));
-        int opt_val = 0;
-        socklen_t opt_len = sizeof(opt_len);
-        getsockopt(fd, SOL_SOCKET, SO_SNDBUF, (char*)&opt_val, &opt_len);
-        BUF_SIZE = opt_val;
-        getsockopt(fd, SOL_SOCKET, SO_RCVBUF, (char*)&opt_val, &opt_len);
-        BUF_SIZE = opt_val > BUF_SIZE ? BUF_SIZE : opt_val;
-
-        fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0) | O_NONBLOCK); // 设置服务器fd为非阻塞
-        struct sockaddr_in addr;
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = INADDR_ANY;
-        addr.sin_port = htons(port);
-        socklen_t addr_len = sizeof(addr);
-        if (bind(fd, (struct sockaddr* )&addr, sizeof(addr))) {
-            perror("bind");
-            exit(1);
-        }
-        if (listen(fd, 2)) {
-            perror("listen");
-            exit(1);
-        }
-
-        clients_fd_set = new fd_set;
-        max_fd = 0;
-    };
+    Server(int port);
 
     ~Server() {
         delete clients_fd_set;
     }
 
-    int accept_one() {
-        if(client_set.size() == USER_SIZE) return -1; // 若满, 则不可接受
-        int new_fd = accept(fd, NULL, NULL); // 否则接受一个客户端
-        if(new_fd < 0) return -1; // 说明此时没有可接受连接的客户端
-        fcntl(new_fd, F_SETFL, fcntl(new_fd, F_GETFL, 0) | O_NONBLOCK); // 设置客户端为非阻塞
-        Client* new_client_ptr = new Client(new_fd);
-        client_set.insert(new_client_ptr);
-        
-        printf("<<<<<<<<<<<<<<<<<<<<A user(fd: %2d) has connnected! The current user_num: %2ld.\n", new_fd, client_set.size());
-
-        // 向客户端发送连接的信息
-        new_client_ptr->reader.feed("[Server] Connecting...\n", -2);
-        char prompt[PROMPT_SIZE];
-        sprintf(prompt, "[Server] Connect successfully! Your fd is: %2d\n", new_fd);
-        new_client_ptr->reader.feed(string(prompt), -2);
-    }
-
-    int update_client_set() {
-        for(auto clt_it = client_set.begin(); clt_it != client_set.end();) {
-            if(update_client(**clt_it) == -1) client_set.erase(clt_it++);
-            else clt_it++;
-        }
-    }
-
-    int update_client(Client &clt) {
-        char temp[32]; 
-        ssize_t recv_size = 0;
-        recv_size = recv(clt.fd, temp, sizeof(temp), MSG_PEEK | MSG_DONTWAIT);
-        //printf("[%d] recv_size=%d, errno=%d\n", accept_fds[index], recv_size, errno);
-        if(((errno == EPIPE || errno == ECONNRESET || errno == EBADF) && recv_size < 0) || recv_size == 0) {
-            int fd = clt.fd;
-            close(clt.fd);
-            printf("User(fd:%2d) has exited! The current user_num: %2ld.>>>>>>>>>>>>>>>>>>>>\n", fd, client_set.size()-1);
-            delete &clt;
-            return -1;
-        }
-        return 0;
-    }
-
-    int get_max_fd() {
-        int max_fd = 0;
-        for(auto &&clt : client_set) {
-            if(max_fd < clt->fd) {
-                max_fd = clt->fd;
-            }
-        }
-        return max_fd;
-    }
-
-    void update_fd_set() {
-        FD_ZERO(clients_fd_set);
-        for(auto &&clt : client_set) {
-            FD_SET(clt->fd, clients_fd_set);
-        }
-    }
-
-    int recv_all_once() {
-        update_fd_set();
-        if(select(get_max_fd() + 1, clients_fd_set, NULL, NULL, &timeout) <= 0) return -1; // 没有可读, 返回
-        for(auto &&clt : client_set) {
-            string recv_buf = clt->recv_some(clients_fd_set);
-            // 更新所有其他客户端的发送队列
-            for(auto &&clt_tofeed : client_set) {
-                if(clt_tofeed == clt) continue;
-                clt_tofeed->reader.feed(recv_buf, clt->fd);
-            }
-        }
-    }
-
-    int send_all_once() {
-        update_fd_set();
-        if(select(get_max_fd() + 1, NULL, clients_fd_set, NULL, &timeout) <= 0) return -1; // 没有可发送客户端, 返回
-        for(auto &&clt : client_set) {
-            clt->send_some(clients_fd_set);
-        }
-    }
+    int accept_one();
+    void update_client_set();
+    int update_client(Client &clt);
+    int get_max_fd();
+    void update_fd_set();
+    int recv_all_once();
+    int send_all_once();
 };
 
 Server* server_ptr;
@@ -235,4 +93,160 @@ int main(int argc, char **argv) {
         server_ptr->update_client_set();
     }
     return 0;
+}
+
+void MessageReader::feed(string data, int from) {
+    buffer += data;
+    // 按照'\n'分割字符串
+    while(true) {
+        size_t pos = buffer.find('\n');
+        if(pos == string::npos) break;
+        char prompt[PROMPT_SIZE];
+        if(from != -2) sprintf(prompt, "[Message from %2d] ", from);
+        else memset(prompt, 0, PROMPT_SIZE*sizeof(char));
+        msgs.push_back(Message(string(prompt) + buffer.substr(0UL, pos + 1), from));
+        if(pos + 1 == buffer.length()) buffer = string();
+        else buffer = buffer.substr(pos + 1);
+    }
+}
+
+Message MessageReader::get_one_msg() {
+    if(msgs.size() <= 0) return Message();
+    Message msg = msgs[0];
+    msgs.erase(msgs.begin());
+    return msg;
+}
+
+string Client::recv_some(fd_set* recv_clients) {
+    if(!FD_ISSET(fd, recv_clients)) return string();
+    char* buffer = new char[BUF_SIZE+1];
+    recv(fd, buffer, BUF_SIZE, 0);
+    string string_buffer = string(buffer);
+    delete [] buffer;
+    return string_buffer;
+}
+
+void Client::send_some(fd_set* send_clients) {
+    if(!FD_ISSET(fd, send_clients)) return; // 不能发送, 返回
+    Message msg;
+    if(send_queue.length() == 0) {
+        msg = reader.get_one_msg();
+        send_queue = msg.buffer;
+    }
+    if(send_queue.length() == 0) return; // 没有可发送消息, 返回
+    ssize_t sent_size = send(fd, send_queue.c_str(), send_queue.length(), MSG_NOSIGNAL);
+    if(sent_size == -1) return;
+    send_queue = send_queue.substr(sent_size);
+}
+
+Server::Server(int port) {
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    int on = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int));
+    int opt_val = 0;
+    socklen_t opt_len = sizeof(opt_len);
+    getsockopt(fd, SOL_SOCKET, SO_SNDBUF, (char*)&opt_val, &opt_len);
+    BUF_SIZE = opt_val;
+    getsockopt(fd, SOL_SOCKET, SO_RCVBUF, (char*)&opt_val, &opt_len);
+    BUF_SIZE = opt_val > BUF_SIZE ? BUF_SIZE : opt_val;
+
+    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0) | O_NONBLOCK); // 设置服务器fd为非阻塞
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+    socklen_t addr_len = sizeof(addr);
+    if (bind(fd, (struct sockaddr* )&addr, sizeof(addr))) {
+        perror("bind");
+        exit(1);
+    }
+    if (listen(fd, 2)) {
+        perror("listen");
+        exit(1);
+    }
+
+    clients_fd_set = new fd_set;
+    max_fd = 0;
+};
+
+
+int Server::accept_one() {
+    if(client_set.size() == USER_SIZE) return -1; // 若满, 则不可接受
+    int new_fd = accept(fd, NULL, NULL); // 否则接受一个客户端
+    if(new_fd < 0) return -1; // 说明此时没有可接受连接的客户端
+    fcntl(new_fd, F_SETFL, fcntl(new_fd, F_GETFL, 0) | O_NONBLOCK); // 设置客户端为非阻塞
+    Client* new_client_ptr = new Client(new_fd);
+    client_set.insert(new_client_ptr);
+    
+    printf("<<<<<<<<<<<<<<<<<<<<A user(fd: %2d) has connnected! The current user_num: %2ld.\n", new_fd, client_set.size());
+
+    // 向客户端发送连接的信息
+    new_client_ptr->reader.feed("[Server] Connecting...\n", -2);
+    char prompt[PROMPT_SIZE];
+    sprintf(prompt, "[Server] Connect successfully! Your fd is: %2d\n", new_fd);
+    new_client_ptr->reader.feed(string(prompt), -2);
+}
+
+void Server::update_client_set() {
+    for(auto clt_it = client_set.begin(); clt_it != client_set.end();) {
+        if(update_client(**clt_it) == -1) client_set.erase(clt_it++);
+        else clt_it++;
+    }
+}
+
+int Server::update_client(Client &clt) {
+    char temp[32]; 
+    ssize_t recv_size = 0;
+    recv_size = recv(clt.fd, temp, sizeof(temp), MSG_PEEK | MSG_DONTWAIT);
+    //printf("[%d] recv_size=%d, errno=%d\n", accept_fds[index], recv_size, errno);
+    if(((errno == EPIPE || errno == ECONNRESET || errno == EBADF) && recv_size < 0) || recv_size == 0) {
+        int fd = clt.fd;
+        close(clt.fd);
+        printf("User(fd:%2d) has exited! The current user_num: %2ld.>>>>>>>>>>>>>>>>>>>>\n", fd, client_set.size()-1);
+        delete &clt;
+        return -1;
+    }
+    return 0;
+}
+
+int Server::get_max_fd() {
+    int max_fd = 0;
+    for(auto &&clt : client_set) {
+        if(max_fd < clt->fd) {
+            max_fd = clt->fd;
+        }
+    }
+    return max_fd;
+}
+
+void Server::update_fd_set() {
+    FD_ZERO(clients_fd_set);
+    for(auto &&clt : client_set) {
+        FD_SET(clt->fd, clients_fd_set);
+    }
+}
+
+int Server::recv_all_once() {
+    update_fd_set();
+    if(select(get_max_fd() + 1, clients_fd_set, NULL, NULL, &timeout) <= 0) return -1; // 没有可读, 返回
+    for(auto &&clt : client_set) {
+        string recv_buf = clt->recv_some(clients_fd_set);
+        // 更新所有其他客户端的发送队列
+        for(auto &&clt_tofeed : client_set) {
+            if(clt_tofeed == clt) continue;
+            clt_tofeed->reader.feed(recv_buf, clt->fd);
+        }
+    }
+}
+
+int Server::send_all_once() {
+    update_fd_set();
+    if(select(get_max_fd() + 1, NULL, clients_fd_set, NULL, &timeout) <= 0) return -1; // 没有可发送客户端, 返回
+    for(auto &&clt : client_set) {
+        clt->send_some(clients_fd_set);
+    }
 }
